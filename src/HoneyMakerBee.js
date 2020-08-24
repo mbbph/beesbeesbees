3473f7195438d8ccf3daebe9bcd146c568b867b0
Lisa Hou <lqnhou@gmail.com>
2020-08-24 12:15:10 -0700

    Complete. Implemented bee hierarchy

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.color = 'yellow';
+  this.job = 'make honey'
+  this.honeyPot = 0;
+
 };
 
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+//NOTE: FUNCTIONS MUST BE DECLARED AFTER PROTOTYPE IS ESTABLISHED
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
